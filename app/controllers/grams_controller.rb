c3d714912eaad2f38b9057a66cdb87def4d016f4
Melanie Marshall <melmacmar@gmail.com>
2016-05-18 08:05:08 +0000

    Fixed Gram spec still 7 failures

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,25 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
+  def update
+  @gram = Gram.find_by_id(params[:id])
+  return render_not_found if @gram.blank?
+  
+  @gram.update_attributes(gram_params)
+  
+  if @gram.valid?
+    redirect_to root_path
+  else
+    return render :edit, status: :unprocessable_entity
+  end
+  
+  if @gram.valid?
+    redirect_to root_path
+  else
+    return render :edit, status: :unprocessable_entity
+  end
+
+  
   def new
     @gram = Gram.new
   end
@@ -10,12 +29,16 @@ class GramsController < ApplicationController
   
 
  def show
-  @gram = Gram.find_by_id(params[:id])
-  if @gram.blank?
-    render text: 'Not Found :(', status: :not_found
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
   end
-end
   
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+  end
+
   def create
     @gram = Gram.create(gram_params.merge(user: current_user))
     if @gram.valid?
@@ -30,5 +53,10 @@ end
   def gram_params
     params.require(:gram).permit(:message)
   end
+   
+  def render_not_found
+    render text: 'Not Found :(', status: :not_found
+  end
   
+end
 end
